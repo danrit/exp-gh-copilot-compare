2d9e6f4d843e61b379a33197be9c4a457c01f2f1
Gabriel Damour <gabriel.damour@carsguide.com.au>
2026-02-16 12:39:19 +1100

    Prepare round 4: begin of upload process

--- upload.py
+++ upload.py
@@ -1,3 +1,19 @@
 """
 Upload the list of files to the AWS S3 bucket.
 """
+
+# settings:
+CSV_FILE_PATH = 'data/export.lite.csv'
+
+TIMESTAMP_FORMAT = '%Y%m%d-%H%M%S'
+
+# SKIPPED_PREFIX path prefix in publicId file (from cloudinary) not in AWS S3.
+SKIPPED_PREFIX = 'editorial/'
+# EXTENSION_JPEG: this one is a best guess, as cloudinary seems to have remove
+# the actual 'fake' extension the image was uploaded with and replaced it with
+# the actual format of the image: psd. We will assume the original extension
+# was jpg, because it seems to be the most common one in the dataset.
+IMAGE_EXTENSION = 'jpg' #
+# BACKUP_EXTENSION: all the files are PSD file originally. Prior to
+# replacing the original by a real jpg version, we are doing a backup as PSD.
+BACKUP_EXTENSION = 'psd'
